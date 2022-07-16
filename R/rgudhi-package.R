ffe60490096fc3942289f8696accce828d5e1f5d
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-07-16 17:03:10 +0200

    Silence CRAN notes.

--- R/rgudhi-package.R
+++ R/rgudhi-package.R
@@ -2,6 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom mathjaxr preview_rd
+#' @importFrom R6 R6Class
 #' @importFrom Rdpack reprompt
+#' @importFrom rlang set_names
+#' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
