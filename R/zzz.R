069e60a0e1f143f5419ed9868bae0d8008d3e22a
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-10-18 15:54:27 +0200

    Force conda install for gudhi.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,3 @@
-# global reference to matplotlib.pyplot (will be initialized in .onLoad)
-plt <- NULL
 # global reference to gudhi (will be initialized in .onLoad)
 gd <- NULL
 # global reference to gudhi.representations (will be initialized in .onLoad)
@@ -7,8 +5,6 @@ gdr <- NULL
 
 .onLoad <- function(libname, pkgname) {
   reticulate::configure_environment(pkgname)
-  # use superassignment to update global reference to matplotlib.pyplot
-  plt <<- reticulate::import("matplotlib.pyplot", delay_load = TRUE)
   # use superassignment to update global reference to gudhi
   gd <<- reticulate::import("gudhi", delay_load = TRUE, convert = TRUE)
   # use superassignment to update global reference to gudhi
