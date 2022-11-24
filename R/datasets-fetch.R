7335563395e20c11d40025d63de8267bb1a12c93
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-11-24 13:50:12 +0100

    Added datasets submodule.

---
+++ R/datasets-fetch.R
@@ -0,0 +1,45 @@
+#' Remote Data Sets
+#'
+#' A collection of function for fetching famous remote data sets.
+#'
+#' @param data_folder A string specifying a location for storing data ot be used
+#'   with GUDHI.
+#' @param accept_license A boolean specifying whether the user accepts the file
+#'   `LICENSE` and prevents from printing the corresponding license terms.
+#'   Defaults to `FALSE`.
+#'
+#' @return A numeric array storing the points of the corresponding data set.
+#' @name fetch
+#'
+#' @examples
+#' b <- fetch_bunny("~/gudhi_data")
+#' s <- fetch_spiral_2d("~/gudhi_data")
+#' clear_data_home("~/gudhi_data")
+NULL
+
+#' @section Stanford bunny dataset:
+#' The [fetch_bunny()] function returns a numeric array of shape \eqn{35947
+#' \times 3}.
+#' @export
+#' @rdname fetch
+fetch_bunny <- function(data_folder, accept_license = FALSE) {
+  withr::local_envvar(c("GUDHI_DATA" = data_folder))
+  gd$datasets$remote$fetch_bunny(accept_license = accept_license)
+}
+
+#' @section `spiral_2d` dataset:
+#' The [fetch_spiral_2d()] function returns a numeric array of shape
+#' \eqn{114,562 \times 2}.
+#' @export
+#' @rdname fetch
+fetch_spiral_2d <- function(data_folder) {
+  withr::local_envvar(c("GUDHI_DATA" = data_folder))
+  gd$datasets$remote$fetch_spiral_2d()
+}
+
+#' @export
+#' @rdname fetch
+clear_data_home <- function(data_folder) {
+  withr::local_envvar(c("GUDHI_DATA" = data_folder))
+  gd$datasets$remote$clear_data_home()
+}
---
+++ R/datasets-generators.R
@@ -0,0 +1,69 @@
+#' Sampling on the Sphere
+#'
+#' The function [sphere()] enables uniform sampling of random *i.i.d.* points on
+#' a \eqn{(d-1)}-sphere in \eqn{\mathbb{R}^d}. The user should provide the
+#' number of points `n_samples` to be generated on the sphere and the ambient
+#' dimension `ambient_dim`. The radius of the sphere is optional and is equal to
+#' `1` by default. Only random points generation is currently available.
+#'
+#' @param n_samples An integer value specifying the sample size.
+#' @param ambient_dim An integer value specifying the dimension of the ambient
+#'   space.
+#' @param radius A numeric value specifying the radius of the sphere. Defaults
+#'   to `1.0`.
+#'
+#' @return A numeric array of shape \eqn{\mathrm{n_samples} \times
+#'   \mathrm{ambient_dim}} storing `n_samples` points uniformly sampled on the
+#'   sphere of dimension `ambient_dim - 1`.
+#'
+#' @export
+#' @examples
+#' sphere(10, 2)
+sphere <- function(n_samples, ambient_dim, radius = 1.0) {
+  n_samples <- as.integer(n_samples)
+  ambient_dim <- as.integer(ambient_dim)
+  gd$datasets$generators$points$sphere(
+    n_samples = n_samples,
+    ambient_dim = ambient_dim,
+    radius = radius
+  )
+}
+
+#' Sampling on the Torus
+#'
+#' The user should provide the number of points `n_samples` to be generated on
+#' the torus and the dimension `dim` of the torus on which points would be
+#' generated in \eqn{\mathbb{R}^{2 \mathrm{dim}}}. The `sample` argument is
+#' optional and is set to `"random"` by default. The generated points are
+#' returned as an array of shape \eqn{\mathrm{n_samples} \times \mathbb{R}^{2
+#' \mathrm{dim}}}.
+#'
+#' @param n_samples An integer value specifying the sample size.
+#' @param dim An integer value specifying the dimension \eqn{\mathbb{R}^{2
+#'   \mathrm{dim}}} of the torus.
+#' @param sample A string specifying the sampling type. Choices are `"random"`
+#'   or `"grid"`. Defaults to `"random"`.
+#'
+#' @return A numeric array of shape \eqn{\mathrm{n_samples} \times \mathbb{R}^{2
+#'   \mathrm{dim}}} storing the sampled points.
+#'
+#' @export
+#' @examples
+#' torus(10, 1)
+torus <- function(n_samples, dim, sample = c("random", "grid")) {
+  n_samples <- as.integer(n_samples)
+  dim <- as.integer(dim)
+  sample <- rlang::arg_match(sample)
+  if (sample == "random" && n_samples > 150 && dim <= 5)
+    gd$datasets$generators$points$torus(
+      n_samples = n_samples,
+      dim = dim,
+      sample = sample
+    )
+  else
+    gd$datasets$generators$points$ctorus(
+      n_samples = n_samples,
+      dim = dim,
+      sample = sample
+    )
+}
