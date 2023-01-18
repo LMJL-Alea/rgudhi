3aed267f91a8228d23b26c4127cd26627c672b3d
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2023-01-18 17:16:48 +0100

    Simplify unit tests for tomato.

--- tests/testthat/test-clustering.R
+++ tests/testthat/test-clustering.R
@@ -1,14 +1,9 @@
 test_that("Clustering algoritm Tomato works", {
   X <- seq_circle(100)
   cl <- Tomato$new()
-  m <- c(0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-         3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-         5, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2,
-         2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0,
-         0, 0, 0, 0)
-  expect_equal(cl$fit_predict(X), m)
+  expect_equal(length(unique(cl$fit_predict(X))), 6)
   cl$set_n_clusters(2)
-  expect_equal(cl$get_labels(), c(rep(0, 25), rep(1, 58), rep(0, 17)))
+  expect_equal(length(unique(cl$get_labels())), 2)
 })
 
 test_that("Visualization works for Tomato", {
